b376d9ef9809e8ef150caa9a791936bef386c3c5
Aidan Baker <abaker@F2N011.42adel.org.au>
2021-10-20 16:05:30 +1030

    test template

---
+++ template.c
@@ -0,0 +1,25 @@
+#include ""
+#include "tester.h"
+
+typedef struct s_case{
+	
+} t_case;
+
+t_case <name>_tests[] = {
+
+};
+
+int tests_<name>()
+{
+	return (arraysize(<name>_tests));
+}
+
+bool exists_<name>()
+{
+	return (ft_<name> != NULL);
+}
+
+void	test_<name>(int n)
+{
+	
+}
