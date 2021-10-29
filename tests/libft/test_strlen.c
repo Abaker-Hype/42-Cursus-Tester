07ade72482851785414aac31ee986d337d159856
Aidan Baker <abaker@F3C011.42adel.org.au>
2021-10-29 17:23:29 +1030

    Rebuilt test info

--- tests/libft/test_strlen.c
+++ tests/libft/test_strlen.c
@@ -27,7 +27,8 @@ void	test_strlen(int n, bool detail)
 {
 	bool pass = true;
 	t_case test = strlen_tests[n];
-	if (detail) testinfo("s", n + 1, test.str);
+	if (detail && n != 5) testinfo("s", n + 1, test.str);
+	else if (detail && n == 5) testinfo("v", 6, test.str, 9);
 	int result = ft_strlen(test.str);
 	int expected = strlen(test.str);
 	if (result != expected) pass = false;
--- tests/template.c
+++ tests/template.c
@@ -1,7 +1,7 @@
 #include "tester.h"
 
 typedef struct s_case{
-	char *input;	
+		
 } t_case;
 
 t_case <name>_tests[] = {
@@ -22,8 +22,9 @@ void	test_<name>(int n, bool detail)
 {
 	bool pass = true;
 	t_case test = <name>_tests[n];
-	if (detail) cprintf(TESTINFO, LBLUE, DEFAULT, YELLOW, n + 1, LBLUE, RED, test.input);
+	if (detail) testinfo("", n + 1,...);
 	//Tests here
-	if (detail) cprintf(TEST<type>RSLT, LBLUE, DEFAULT, YELLOW, result, LBLUE, YELLOW, expected);
+	if (result != expected) pass = false;
+	if (detail) resultinfo("", result, expected);
 	if (pass) setgrade(PASS);
 }
