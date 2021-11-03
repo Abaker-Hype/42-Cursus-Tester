8250317e72000849083426d76c25ff9b657802c0
Aidan Baker <abaker@F2N011.42adel.org.au>
2021-11-03 15:17:28 +1030

    updated frontend/args

--- utils/launcher.c
+++ utils/launcher.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	if (!parseargs(argc, argv)) return (0);
-	if (args.func) cprintf(RUNTEST, LBLUE, DEFAULT, args.func->name);
+	if (args.func) cprintf("Running test/s for %s\n", LBLUE, DEFAULT, args.func->name);
 	else cprintf("Running test/s for %s\n",PINK, DEFAULT, argv[1]);
 	loadcolumns();
 	testhandler();
